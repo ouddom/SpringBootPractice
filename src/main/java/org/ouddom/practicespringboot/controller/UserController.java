1414da6af3feb74f24ebd119ec9f5d2779c488c5
Tith Ouddom <ouddomtith.com@gmail.com>
2024-06-21 16:43:33 +0700

    Add Web config

--- src/main/java/org/ouddom/practicespringboot/controller/UserController.java
+++ src/main/java/org/ouddom/practicespringboot/controller/UserController.java
@@ -2,7 +2,6 @@ package org.ouddom.practicespringboot.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
